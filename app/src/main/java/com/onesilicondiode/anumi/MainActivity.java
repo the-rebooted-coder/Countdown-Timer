d610f022ca631f76a41fac59697f600ce7cc35bd
Spandan Saxena <spandansaxena@gmail.com>
2023-08-13 16:30:44 +0530

    Added FAB button and haptics on button press
    Subtle FAB animation added too

--- app/src/main/java/com/onesilicondiode/anumi/MainActivity.java
+++ app/src/main/java/com/onesilicondiode/anumi/MainActivity.java
@@ -1,11 +1,19 @@
 package com.onesilicondiode.anumi;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.javiersantos.appupdater.AppUpdater;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.Calendar;
 import java.util.Locale;
@@ -13,15 +21,24 @@ import java.util.concurrent.TimeUnit;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    private Vibrator vibrator;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         startCountdownService();
         TextView countdownTextView = findViewById(R.id.countdownTextView);
+        FloatingActionButton updateApp = findViewById(R.id.updateApp);
+        vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+        updateApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Perform a pop animation when the FAB is clicked
+                handleFabClick(view);
+            }
+        });
 
-        // Set the target date and time (30 August 2023, 12:00 AM)
+    // Set the target date and time (30 August 2023, 12:00 AM)
         Calendar targetDate = Calendar.getInstance();
         targetDate.set(2023, Calendar.AUGUST, 29, 0, 0, 0);
         Calendar currentDate = Calendar.getInstance();
@@ -62,5 +79,35 @@ public class MainActivity extends AppCompatActivity {
     private void startCountdownService() {
         Intent serviceIntent = new Intent(this, CountdownService.class);
         startService(serviceIntent);
+    }
+    private void handleFabClick(View view) {
+        float scaleFactor = 1.2f;
+        long duration = 200; // Animation duration in milliseconds
+        long[] pattern = {0, 100, 100, 100, 200, 100};
+        updateDevice();
+        if (vibrator != null && vibrator.hasVibrator()) {
+            vibrator.vibrate(VibrationEffect.createWaveform(pattern, -1));
+        }
+        // Scale up
+        view.animate()
+                .scaleX(scaleFactor)
+                .scaleY(scaleFactor)
+                .setDuration(duration)
+                .setInterpolator(new AccelerateInterpolator())
+                .withEndAction(() -> {
+                    // Scale back to the original size
+                    view.animate()
+                            .scaleX(1.0f)
+                            .scaleY(1.0f)
+                            .setDuration(duration)
+                            .setInterpolator(new AccelerateInterpolator())
+                            .start();
+                })
+                .start();
+    }
+
+    private void updateDevice() {
+        AppUpdater appUpdater = new AppUpdater(this);
+        appUpdater.start();
     }
 }
