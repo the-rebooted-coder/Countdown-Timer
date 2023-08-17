05367cd245ddfa635eeb3b8a179b729c3288e554
Spandan Saxena <spandansaxena@gmail.com>
2023-08-17 23:32:03 +0530

    App Locks Flawlessly

--- app/src/main/java/com/onesilicondiode/anumi/LockApp.java
+++ app/src/main/java/com/onesilicondiode/anumi/LockApp.java
@@ -1,14 +1,27 @@
 package com.onesilicondiode.anumi;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.security.keystore.KeyGenParameterSpec;
 import android.security.keystore.KeyProperties;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.hardware.fingerprint.FingerprintManagerCompat;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.security.KeyStore;
 
@@ -22,7 +35,10 @@ public class LockApp extends AppCompatActivity {
     private static final String KEY_NAME = "my_key_name";
     public static final String APP_LOCK = "lockedApp";
     public static final String APP_IS_UNLOCKED = "appUnlocked";
+    private Vibrator vibrator;
     private FingerprintManagerCompat fingerprintManager;
+    private MaterialButton usePinButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +58,47 @@ public class LockApp extends AppCompatActivity {
                 // Fingerprint authentication not available, use PIN as a fallback
                 authenticateWithPIN(CORRECT_PIN);
             }
+            usePinButton = findViewById(R.id.btnUsePin);
+
+            usePinButton.setOnClickListener(v -> showPinInputDialog());
         }
+    }
+    private void showPinInputDialog() {
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View dialogView = inflater.inflate(R.layout.dialog_pin_input, null);
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this)
+                .setTitle("Enter PIN")
+                .setView(dialogView)
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        TextInputEditText pinEditText = dialogView.findViewById(R.id.appUnlockPin);
+                        String enteredPin = pinEditText.getText().toString();
+                        if (enteredPin.equals("2908")) {
+                            navigateToMainActivity();
+                        } else {
+                            long[] pattern = {0, 100, 100, 100, 200, 100};
+                            if (vibrator != null && vibrator.hasVibrator()) {
+                                vibrator.vibrate(VibrationEffect.createWaveform(pattern, -1));
+                            }
+                            shakeDialogAndShowError();
+                        }
+                    }
+                })
+                .setNegativeButton("CANCEL", null);
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+    private void navigateToMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+    private void shakeDialogAndShowError() {
+        Toast.makeText(this,"Uh-huh Wrong PIN 🤦‍♂️", Toast.LENGTH_SHORT).show();
     }
     private void authenticateWithFingerprint() {
         try {
